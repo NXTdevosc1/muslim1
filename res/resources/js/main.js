8e14434f26d9025cf2443b0df24f4cd041410ed1
devos_c <loukmane.khaled@gmail.com>
2023-06-30 18:17:18 +0100

    socket communication

--- res/resources/js/main.js
+++ res/resources/js/main.js
@@ -1,8 +1,71 @@
 /*
  Javascript check file
 */
+const MESSAGE_CONNECT = 0
+const MESSAGE_SEND = 1
+const MESSAGE_NOTIFY = 2
+const MESSAGE_DISCONNECT = 3
 
-var UserLoggedIn = false;
-if(localStorage.getItem("token")) {
+const parseCookie = str =>
+  str
+  .split(';')
+  .map(v => v.split('='))
+  .reduce((acc, v) => {
+    acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+    return acc;
+  }, {});
+
+
+var cookies = {};
+
+try {
+    if(document.cookie.length) cookies = parseCookie(document.cookie);
+} catch(err) {
+    alert("Invalid cookies, please delete them");
+}
+
+console.log("cookies:", cookies, "token", cookies.token);
+
+if(cookies.token) {
     
+    let Socket = new WebSocket("ws://localhost:5000");
+    
+    Socket.onopen = ((ev) => {
+        console.log("OPEN", (ev));
+        Socket.send((JSON.stringify({Message: MESSAGE_CONNECT, token: cookies.token})));
+    })
+    
+    Socket.onmessage = ((ev) => {
+        const data = JSON.parse(ev);
+        switch(data.Message) {
+            case MESSAGE_NOTIFY:
+                {
+                    console.log("Notify:", data);
+                    break;
+                }
+            case MESSAGE_DISCONNECT:
+                {
+                    alert("Server wants to disconnect the client.");
+                    document.cookie = '';
+                    document.location.reload();
+                    break;
+                }
+        }
+        if(data.Message == MESSAGE_DISCONNECT) {
+
+        }
+        console.log("MESSAGE", data);
+    })
+    
+    Socket.onclose = ((ev) => {
+        alert("Server connection died.");
+        document.location.reload();
+    })
+    
+    
+    Socket.onerror = ((ev) => {
+        alert("Server connection error.");
+        document.location.reload();
+    })
+
 }
--- srvws.js
+++ srvws.js
@@ -1,20 +1,78 @@
 const websocket = require('ws')
+const {db} = require("./srvif")
 const wss = new websocket.Server({
     port: 5000
 });
 
-const {uuidv4} = require("uuid")
+const MESSAGE_CONNECT = 0
+const MESSAGE_SEND = 1
+const MESSAGE_NOTIFY = 2
+const MESSAGE_DISCONNECT = 3
+
+const { v4: uuidv4 } = require('uuid');
 
 const clients = new Map();
 
+
+function HandleClientMessage(message) {
+
+}
+
 wss.on("connection", (ws) => {
+    console.log("Socket connection");
     const id = uuidv4();
-    const data = {
-        id
+    const userdata = {
+        connectionid: id,
+        user: undefined,
+        NumMessages: 0,
     };
-    clients.set(ws, data);
+    clients.set(ws, userdata);
+
+    ws.on("close", (code, reason) => {
+        clients.delete(ws);
+        console.log("Connection closed.");
+    })
+    ws.on("message", (Message) => {
+        var data;
+        try {
+            data = JSON.parse(Message);
+            console.log(data);
+            if(typeof data.Message != "number" || data.Message != MESSAGE_CONNECT || typeof data.token != 'string' || data.token.length != 36) {
+                console.log(typeof data.Message, data.Message, typeof data.token, data.token.length);
+                ws.close();
+                return;
+            }
+        } catch(err) {
+            console.log("Server processing error, invalid socket message");
+            ws.close();
+            return;
+        }
+
+        console.log("Checking the token...", data);
+        db.query("SELECT * FROM `tokens` WHERE token = ?", [data.token], (err, dbres) => {
+            if(err) throw err;
+            if(!dbres.length) {
+                // Token probably expired
+                ws.send(JSON.stringify({Message: 0}));
+            } else {
+                db.query("SELECT * FROM `users` WHERE userid = ?", [dbres[0].userid], (err, dbres1) => {
+                    userdata.user = dbres1[0];
+                    console.log("socket connection successfull, userdata :", userdata);
+                })
+            }
+            })
+    });
+    ws.on("error", (err) => {
+        console.log("socket error");
+    })
+
+
 })
 
-wss.on("close", (ws) => {
-    clients.delete(ws);
+
+
+wss.on("error", (ws) => {
+    console.log("Socket error");
+    throw ws;
 })
+
